2c3c0121bdc4a57f7edfb402387d0bcd1b56d60e
Isaac Santana Souza <isaacsantana.162@gmail.com>
2026-03-22 11:10:40 -0300

    Added method to display available options

--- main.py
+++ main.py
@@ -12,6 +12,17 @@ class StringCleaner:
 		self.raw_string = ''
 		self.final_string = ''
 
+		self.options = {
+			'0' : ['quit', self.quit],
+			'1' : ['keep only letters', self.keep_letters],
+			'2' : ['keep only numbers', self.keep_numbers],
+			'3' : ['keep only letters and numbers', self.keep_letters_and_numbers]
+		}
+
+	def print_options(self):
+		for key, value in self.options.items():
+			print(f'{key} - {value[0]}')
+
 
 	def keep_letters(self, string):
 		result = ''
@@ -46,12 +57,7 @@ class StringCleaner:
 	def start(self):
 		loop = True
 
-		options = {
-			'0' : ['quit', self.quit],
-			'1' : ['keep only letters', self.keep_letters],
-			'2' : ['keep only numbers', self.keep_numbers],
-			'3' : ['keep only letters and numbers', self.keep_letters_and_numbers]
-		}
+
 
 		if use_clipboard:
 			self.raw_string = clipboard.paste()
@@ -62,23 +68,18 @@ class StringCleaner:
 
 		print('Choose an action:\n')
 
-		for key, value in options.items():
-			print(f'{key} - {value[0]}')
+		self.print_options()
 
 
 		while loop:
 			action = str(input('\n> '))
 
-			if action in options.keys():
-				self.final_string = options[action][1](self.raw_string)
+			if action in self.options.keys():
+				self.final_string = self.options[action][1](self.raw_string)
 				loop = False
 			else:
-				print(f"Wrong option! Try again. Valid options are [", end=' ')
-				
-				for key in options.keys():
-					print(key, end=' ')
-
-				print(']')
+				print(f"Wrong option! Try again. Valid options are:\n")
+				self.print_options()
 
 		if use_clipboard:
 			print(f'Final string copied to clipboard: \n\n{self.final_string}')
