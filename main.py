df2edc2c05a8922463f2dee019d1a25fe64a0f72
Isaac Santana Souza <isaacsantana.162@gmail.com>
2026-03-22 10:51:51 -0300

    Refactor the code to use OOP

--- main.py
+++ main.py
@@ -1,89 +1,95 @@
-use_clipboard = False
-
 try:
 	import clipboard
 
 	use_clipboard = True
 except ModuleNotFoundError:
+	use_clipboard = False
 	print("Clipboard module not installed!")
 
 
-raw_string = ''
-final_string = ''
-mode = ''
-loop = True
+class StringCleaner:
+	def __init__(self):
+		self.raw_string = ''
+		self.final_string = ''
 
 
-def keep_letters(string):
-	result = ''
+	def keep_letters(self, string):
+		result = ''
 
-	for char in string:
-		if char.isalpha() or char.isspace():
-			result += char
+		for char in string:
+			if char.isalpha() or char.isspace():
+				result += char
 
-	return result
-
-
-def keep_numbers(string):
-	result = ''
-
-	for char in string:
-		if char.isnumeric():
-			result += char
-
-	return result
+		return result
 
 
-def keep_letters_and_numbers(string):
-	result = ''
+	def keep_numbers(self, string):
+		result = ''
 
-	for char in string:
-		if char.isalnum() or char.isspace():
-			result += char
+		for char in string:
+			if char.isnumeric():
+				result += char
 
-	return result
-
-
-def quit(_):
-	exit()
-
-options = {
-	'0' : ['quit', quit],
-	'1' : ['keep only letters', keep_letters],
-	'2' : ['keep only numbers', keep_numbers],
-	'3' : ['keep only letters and numbers', keep_letters_and_numbers]
-}
+		return result
 
 
-if use_clipboard:
-	raw_string = clipboard.paste()
+	def keep_letters_and_numbers(self, string):
+		result = ''
 
-	print(f'Your string:\n\n{raw_string}\n')
-else:
-	raw_string = str(input("Paste or type the string: ")).strip()
+		for char in string:
+			if char.isalnum() or char.isspace():
+				result += char
 
-print('Choose an action:\n')
+		return result
 
-for key, value in options.items():
-	print(f'{key} - {value[0]}')
+	
+	def start(self):
+		loop = True
+
+		options = {
+			'0' : ['quit', self.quit],
+			'1' : ['keep only letters', self.keep_letters],
+			'2' : ['keep only numbers', self.keep_numbers],
+			'3' : ['keep only letters and numbers', self.keep_letters_and_numbers]
+		}
+
+		if use_clipboard:
+			self.raw_string = clipboard.paste()
+
+			print(f'Your string:\n\n{self.raw_string}\n')
+		else:
+			self.raw_string = str(input("Paste or type the string: ")).strip()
+
+		print('Choose an action:\n')
+
+		for key, value in options.items():
+			print(f'{key} - {value[0]}')
 
 
-while loop:
-	mode = str(input('\n> '))
+		while loop:
+			action = str(input('\n> '))
 
-	if mode in options.keys():
-		final_string = options[mode][1](raw_string)
-		loop = False
-	else:
-		print(f"Wrong option! Try again. Valid options are [", end=' ')
-		
-		for key in options.keys():
-			print(key, end=' ')
+			if action in options.keys():
+				self.final_string = options[action][1](self.raw_string)
+				loop = False
+			else:
+				print(f"Wrong option! Try again. Valid options are [", end=' ')
+				
+				for key in options.keys():
+					print(key, end=' ')
 
-		print(']')
+				print(']')
 
-if use_clipboard:
-	print(f'Final string copied to clipboard: \n\n{final_string}')
-	clipboard.copy(final_string)
-else:
-	print(f'Final String:\n\n{final_string}')
+		if use_clipboard:
+			print(f'Final string copied to clipboard: \n\n{self.final_string}')
+			clipboard.copy(self.final_string)
+		else:
+			print(f'Final String:\n\n{self.final_string}')
+
+
+	def quit(self, _):
+		exit()
+
+
+string_cleaner = StringCleaner()
+string_cleaner.start()
